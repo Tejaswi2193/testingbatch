b159f02aee38e65c674de46f3182db1e9b1fd298
ASUS <ASUS@LAPTOP-4LAMJMI4>
2020-12-17 22:07:00 +0530

    commit by tejaswi12

--- GitHub Project/src/github_dec17/Demo_GitHub.java
+++ GitHub Project/src/github_dec17/Demo_GitHub.java
@@ -2,6 +2,10 @@ package github_dec17;
 
 public class Demo_GitHub 
 {
+	public void sum()
+	{
+		System.out.println("sum-----Test");
+	}
 public static void main(String[] args) 
 {
 	System.out.println("Hello GitHub");
